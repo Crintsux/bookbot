7cae8df280b5dec72c9ff9e153ae5bf055e849a4
Crintsux <fifa.d@seznam.cz>
2024-04-21 16:57:07 +0200

    print_report() function implemented

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(get_word_count(text))
+    #print(f"The word count is: {get_word_count(text)}")
+    #print(get_char_count(text))
+    print_report(text, book_path)
+    
 
 def get_word_count(text):
     words = text.split()
@@ -14,7 +17,31 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_letter_count(text):
-    pass
+def get_char_count(text):
+    lowered_text = text.lower()
+    output_dict = {}
+    for char in lowered_text:
+        if char not in output_dict:
+            output_dict[char] = 1
+        else:
+            output_dict[char] += 1
+    return output_dict
 
+def get_value(dict):
+    return dict["num"]
+
+def print_report(text, book):
+    dict = get_char_count(text)
+    character_list = []
+    for item in dict:
+        if item.isalpha():
+            temp_dict = {}
+            temp_dict["character"] = item
+            temp_dict["num"] = dict[item]
+            character_list.append(temp_dict)
+    character_list.sort(reverse=True, key=get_value)
+    print(f"--- Begin report of {book} ---")
+    print(f"{get_word_count(text)} words were found in the document")
+    for rec in character_list:
+        print(f"The '{rec["character"]}' character was found {rec["num"]} times")
 main()
